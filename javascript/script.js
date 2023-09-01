0733c921591db5f944d31359ab7fc376ded9f6cc
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-08-31 23:32:51 -0300

    melhorias js

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,10 @@
 (() => {
     const userState = document.querySelector('#state');
     const btnSend = document.querySelector('.btn-send');
-
     const divState = document.querySelector('#main-state');
+    
+
+    const elemOption = () => document.createElement('option');
 
     //prevent number
     userState.addEventListener('keydown', (e) => {
@@ -26,36 +28,30 @@
     })
 
     btnSend.addEventListener('click', (e) => {
-        searchState('PR');
+        searchState(userState.value.toUpperCase());
         divState.classList.add('hidden');
     })
 
-    function searchState(stateUser) {
+    async function searchState(stateUser) {
         const TOKEN = '383e3b0dfd73c9eab3e5b2fa9bd85150';
-
         const URL = `http://apiadvisor.climatempo.com.br/api/v1/locale/city?state=${stateUser}&token=${TOKEN}`;
 
-        const URLCONFIG = {
-            method: 'POST',
-            cache: 'default'
-        };
-        let city;
-
-        fetch(URL).then(function (RESPONSE) {
-            city = RESPONSE.json();
-            writenAllCityHtml(city);
+        const city = await fetch(URL).then(function (RESPONSE) {
+            return RESPONSE
         })
-
+        const cityConverted = await city.json();
+        writenAllCityHtml(cityConverted);
         const URLCONFIRM = 'http://apiadvisor.climatempo.com.br/api-manager/user-token/:your-app-token/locales';
     };
 
 
     function writenAllCityHtml(jsonCity) {
-        const elemOption = document.createElement('option');
         const allCity = document.querySelector('#all-city');
 
-        console.log('completo array', jsonCity)
-        console.log('chegou aqui', typeof jsonCity.PromiseResult);
+        jsonCity.map((elem)=>{
+            allCity.appendChild(elemOption()).innerHTML = elem.name;
+            
+        })
 
     }
 
