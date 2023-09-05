6cb64731764e8e210b3e05ee7489a823cce12fd9
lucas Pedrolo <lucassamuelpedrolo@gmail.com>
2023-09-04 23:13:53 -0300

    retirando funcao async

--- javascript/script.js
+++ javascript/script.js
@@ -2,11 +2,13 @@
     const userState = document.querySelector('#state');
     const btnSend = document.querySelector('.btn-send');
     const divState = document.querySelector('#main-state');
-    
+    const divCity = document.querySelector('#main-city');
+    const userCity = document.querySelector('#search-city');
+    const btnSendCity = document.querySelector('#btn-send-city');
 
     const elemOption = () => document.createElement('option');
 
-    //prevent number
+    //prevent number in input
     userState.addEventListener('keydown', (e) => {
         if (!(e.keyCode >= 65 && e.keyCode <= 90) && e.keyCode !== 8 && e.keyCode !== 13) {
             e.preventDefault();
@@ -27,35 +29,65 @@
         }
     })
 
+    //send state
     btnSend.addEventListener('click', (e) => {
         searchState(userState.value.toUpperCase());
-        divState.classList.add('hidden');
     })
 
-    async function searchState(stateUser) {
+    userCity.addEventListener('input', (e) => {
+        if (e !== null && e !== ' ') {
+            btnSendCity.disabled = false;
+            btnSendCity.classList.remove('disabled-city');
+        } else {
+            btnSendCity.classList.add('disabled-city');
+            btnSendCity.disabled = true;
+        }
+    })
+
+    //conects with API 'clima tempo'
+    function searchState(stateUser) {
         const TOKEN = '383e3b0dfd73c9eab3e5b2fa9bd85150';
         const URL = `http://apiadvisor.climatempo.com.br/api/v1/locale/city?state=${stateUser}&token=${TOKEN}`;
 
-        const city = await fetch(URL).then(function (RESPONSE) {
-            return RESPONSE
+
+        fetch(URL).then((respose) => {
+            return respose.json()
+        }).then((resposeConverted) => {
+            if(getInvalidStates(resposeConverted)){
+                
+                }  
         })
-        const cityConverted = await city.json();
-        writenAllCityHtml(cityConverted);
-        const URLCONFIRM = 'http://apiadvisor.climatempo.com.br/api-manager/user-token/:your-app-token/locales';
     };
 
 
-    function writenAllCityHtml(jsonCity) {
-        const allCity = document.querySelector('#all-city');
-
-        jsonCity.map((elem)=>{
-            allCity.appendChild(elemOption()).innerHTML = elem.name;
-            
-        })
-
+    function getInvalidStates(dataState) {
+        if (dataState.length <= 0) {
+            alert('Coloque um estado valido');
+            return false;
+        } else {
+            divState.classList.add('hidden');
+            divCity.classList.remove('hidden');
+            writtenAllCityHtml(dataState);
+            return true;
+        }
     }
 
+    function getInvalidCities(dataCity) {
+        console.log('dentro da funcao')
+        const dataFilter = dataCity.filter((elem) => {
+            return userCity.value === elem.nome ? true : false;
+        })
+    }
 
+    //const URLCONFIRM = 'http://apiadvisor.climatempo.com.br/api-manager/user-token/:your-app-token/locales';
 
+    //writes all cities in html
+    function writtenAllCityHtml(jsonCity) {
+        const allCity = document.querySelector('#all-city');
+
+        jsonCity.map((elem) => {
+            allCity.appendChild(elemOption()).innerHTML = elem.name;
+        })
+    }
 
 })();
